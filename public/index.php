f5418ac3f7fc4c6b228fd81be0d92b9b9494d967
cacoch <zdenek@ciencias.unam.mx>
2016-09-26 18:21:20 -0500

    Se me olvido agregar unos archivos cambiados

--- public/index.php
+++ public/index.php
@@ -1,14 +1,19 @@
 <?php 
 
 // Setup autoloader
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 
 // Instantiate Slim
+$settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
+// Register dependencies 
+require __DIR__ . '/../src/dependencies.php';
 
 // Register the routes
+require __DIR__ . '/../src/routes.php';
 
-// Run app
+
+// Run app!!
 $app->run();
